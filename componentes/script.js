1294e510d882fb3136df5b44ce6eeb0a115c88b0
orlando-paulo99 <orlandopaulo8]7@gmail.com>
2026-03-26 22:19:56 -0300

    ajuste menu lateral

---
+++ componentes/script.js
@@ -0,0 +1,9 @@
+let b=document.getElementById("btnMenu")
+let m=document.getElementById("side")
+
+b.addEventListener("click", () =>{
+    console.log("clicou")
+    m.classList.toggle("active")
+
+})
+
--- script.js
+++ script.js
@@ -54,8 +54,18 @@ b.addEventListener("click" , async ()=>{
 
 })
 
-fetch('componentes/menu.html')
+fetch('./componentes/menu.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('menu').innerHTML = data;
-  });
+
+    let b=document.getElementById("btnMenu")
+    let m=document.getElementById("side")
+
+    b.addEventListener("click", () =>{
+        console.log("clicou")
+        m.classList.toggle("active")
+
+    })
+
+});
